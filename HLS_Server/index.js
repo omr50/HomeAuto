a24bc541fb84a07cb7afa49881ccbc31fdc5c1c6
Omr50 <omrab4@gmail.com>
2023-07-22 02:04:07 -0400

    Added camera zoom

--- HLS_Server/index.js
+++ HLS_Server/index.js
@@ -10,6 +10,12 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// test zoom later.
+// move to files based on category.
+// camera motion in its own folder.
+// hls server in its own folder.
+// light controls in its own folder.
+
 
 let device = new onvif.OnvifDevice({
     xaddr: 'http://192.168.0.13:8899/onvif/device_service', // Replace with your camera's ONVIF address
@@ -17,6 +23,8 @@ let device = new onvif.OnvifDevice({
   
 let moveCameraLeft = ()=>{};
 let moveCameraRight = ()=>{};
+let zoomIn = ()=>{};
+let zoomOut = ()=>{};
 
   device.init().then((info) => {
     console.log(info);
@@ -43,6 +51,27 @@ let moveCameraRight = ()=>{};
         'Timeout':3 // time duration the move should continue
       };
   
+      let zoom_in = {
+        'ProfileToken': 'Profile_token1', 
+        'Velocity': { // PTZ velocity. x, y, and zoom values must be between -1 and 1.
+          'x': 0.0, // Pan speed
+          'y': 0.0, // Tilt speed
+          'z': 0.0, 
+          'zoom': -1.0 // Zoom speed
+        },
+        'Timeout':3 // time duration the move should continue
+      };
+
+      let zoom_out = {
+        'ProfileToken': 'Profile_token1', 
+        'Velocity': { // PTZ velocity. x, y, and zoom values must be between -1 and 1.
+          'x': 0.0, // Pan speed
+          'y': 0.0, // Tilt speed
+          'z': 0.0, 
+          'zoom': 1.0 // Zoom speed
+        },
+        'Timeout':3 // time duration the move should continue
+      };
     const delay = ms => new Promise(res => setTimeout(res, ms));
 
     moveCameraLeft = async () => {
@@ -62,6 +91,32 @@ let moveCameraRight = ()=>{};
         for (let i = 0; i < 3; i++) {
           try {
             await device.services.ptz.continuousMove(paramsRight);
+            console.log('Continuous move successful');
+            await delay(300); // 2 seconds delay
+          } catch (err) {
+            console.error(err);
+            break;
+          } 
+        }
+      }
+
+      zoomIn = async () => {
+        for (let i = 0; i < 3; i++) {
+          try {
+            await device.services.ptz.continuousMove(zoom_in);
+            console.log('Continuous move successful');
+            await delay(300); // 2 seconds delay
+          } catch (err) {
+            console.error(err);
+            break;
+          } 
+        }
+      }
+
+      zoomOut = async () => {
+        for (let i = 0; i < 3; i++) {
+          try {
+            await device.services.ptz.continuousMove(zoom_out);
             console.log('Continuous move successful');
             await delay(300); // 2 seconds delay
           } catch (err) {
@@ -109,12 +164,22 @@ app.get('/lights', (req, res) => {
 
 app.get('/move-left', async (req, res) => {
     await moveCameraLeft();
-    res.send('successfully moved camera');
+    res.send('successfully moved camera left');
 })
 
 app.get('/move-right', async (req, res) => {
     await moveCameraRight();
-    res.send('successfully moved camera');
+    res.send('successfully moved camera right');
+})
+
+app.get('/zoom-in', async (req, res) => {
+  await zoomIn();
+  res.send('successfully zoomed in');
+})
+
+app.get('/zoom-out', async (req, res) => {
+  await zoomOut();
+  res.send('successfully zoomed out');
 })
 
 const server = app.listen(3000);
