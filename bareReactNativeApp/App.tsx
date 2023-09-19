82a1b7ddc8312ad3b3420407d1152d07d8ee69f6
Omr50 <omrab4@gmail.com>
2023-09-19 12:42:07 -0400

    Re-vamped Home page

--- bareReactNativeApp/App.tsx
+++ bareReactNativeApp/App.tsx
@@ -5,14 +5,18 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Dimensions,
+  ImageBackground,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -56,43 +60,58 @@ function Section({children, title}: SectionProps): JSX.Element {
 }
 
 function App(): JSX.Element {
+
+  React.useEffect(() => {
+    StatusBar.setBarStyle('light-content');
+    StatusBar.setBackgroundColor('red');
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
+
+  const [textWidth, setTextWidth] = useState(0);
+  const [buttonWidth, setButtonWidth] = useState(0);
+
+  const onTextLayout = (event: any) => {
+    const { width } = event.nativeEvent.layout;
+    setTextWidth(width);
+  };
+
+  const onButtonLayout = (event: any) => {
+    const { width } = event.nativeEvent.layout;
+    setButtonWidth(width);
+  };
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const translateX = -textWidth * 0.5;
+  const translateXButton = -buttonWidth * 0.5;
+
   return (
+    <>
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+          <ImageBackground source={require('./images/smart-home-img.webp')} style={{width: '100%', height: '100%'}}>
+
+
+          </ImageBackground>
+
+          {/* text absolute and button absolute */}
+
+          <Text onLayout={onTextLayout} style={[{ transform: [{ translateX: translateX }] }, styles.text]}>Home Auto</Text>
+          
+          <TouchableOpacity
+          onLayout={onButtonLayout}
+          style={ [{ transform: [{ translateX: translateXButton }] }, styles.enterButton] }
+          onPress={() => {
+            console.log('pressed')
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 25, fontWeight: '500' }}>Enter</Text>
+        </TouchableOpacity>
     </SafeAreaView>
+    </>
   );
 }
 
@@ -113,6 +132,36 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  text: {
+    fontWeight: "100",    
+    position: 'absolute',
+    left: '50%',
+    top: -15,
+    fontSize: 48,
+    color: 'lightblue',
+    textShadowColor: 'red',
+    textShadowOffset: { width: -1, height: -1 },
+    textShadowRadius: 5,
+    textShadow: [
+      { textShadowOffset: { width: -1, height: -1 }, textShadowRadius: 0 },
+      { textShadowOffset: { width: 1, height: -1 }, textShadowRadius: 0 },
+      { textShadowOffset: { width: -1, height: 1 }, textShadowRadius: 0 },
+      { textShadowOffset: { width: 1, height: 1 }, textShadowRadius: 0 }
+    ],
+  },
+  enterButton: {
+    backgroundColor: 'rgba(0,91,150, 0.5)',
+    borderWidth: 1,
+    borderColor: 'pink',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 45,
+    paddingRight: 45,
+    // borderRadius: 45,
+    position: 'absolute',
+    top: '80%',
+    left: '50%',
+  }
 });
 
 export default App;
